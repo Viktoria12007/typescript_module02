f6b6945950c0a00266ab7e148f93fc140fbe09b9
Виктория Голубова <v.golubova@denexy.com>
2024-01-13 19:56:39 +0400

    Changed solution for task5.ts

--- src/task5/task5.ts
+++ src/task5/task5.ts
@@ -1,3 +1,21 @@
 export function deepEqual(a: object, b: object): boolean {
-    return JSON.stringify(a) === JSON.stringify(b);
+    const aKeys = Object.keys(a);
+    const bKeys = Object.keys(b);
+
+    if (aKeys.length !== bKeys.length) {
+        return false;
+    }
+    for (const key of aKeys) {
+        if (!bKeys.includes(key)) {
+            return false;
+        }
+        if (typeof a[key as keyof typeof a] === 'object' && typeof b[key as keyof typeof b] === 'object') {
+            return deepEqual(a[key as keyof typeof a], b[key as keyof typeof b]);
+        }
+        if (a[key as keyof typeof a] !== b[key as keyof typeof b]) {
+            return false;
+        }
+    }
+
+    return true;
 }
